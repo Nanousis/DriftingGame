28046a3865071f18c67ecef5e1e89da4c9abe540
Panagiotis Nanousis <11467288+Nanousis@users.noreply.github.com>
2023-05-24 02:21:17 +0300

    Add files via upload

---
+++ Scripts/Deform.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deform : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float deformRadius = 0.2f;
+    [Range(0, 10)]
+    public float maxDeform = 0.1f;
+    [Range(0, 1)]
+    public float damageFalloff = 1;
+    [Range(0, 10)]
+    public float damageMultiplier = 1;
+    [Range(0, 100000)]
+    public float minDamage = 1;
+
+    public AudioClip[] collisionSounds;
+
+    private MeshFilter filter;
+    private Rigidbody physics;
+    private MeshCollider coll;
+    private Vector3[] startingVerticies;
+    private Vector3[] meshVerticies;
+
+    void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+        physics = GetComponent<Rigidbody>();
+
+        if (GetComponent<MeshCollider>())
+            coll = GetComponent<MeshCollider>();
+
+        startingVerticies = filter.mesh.vertices;
+        meshVerticies = filter.mesh.vertices;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float collisionPower = collision.impulse.magnitude;
+
+        if (collisionPower > minDamage)
+        {
+            if (collisionSounds.Length > 0)
+                AudioSource.PlayClipAtPoint(collisionSounds[Random.Range(0, collisionSounds.Length)], transform.position, 0.5f);
+
+            foreach (ContactPoint point in collision.contacts)
+            {
+                for (int i = 0; i < meshVerticies.Length; i++)
+                {
+                    Vector3 vertexPosition = meshVerticies[i];
+                    Vector3 pointPosition = transform.InverseTransformPoint(point.point);
+                    float distanceFromCollision = Vector3.Distance(vertexPosition, pointPosition);
+                    float distanceFromOriginal = Vector3.Distance(startingVerticies[i], vertexPosition);
+
+                    if (distanceFromCollision < deformRadius && distanceFromOriginal < maxDeform) // If within collision radius and within max deform
+                    {
+                        float falloff = 1 - (distanceFromCollision / deformRadius) * damageFalloff;
+
+                        float xDeform = pointPosition.x * falloff;
+                        float yDeform = pointPosition.y * falloff;
+                        float zDeform = pointPosition.z * falloff;
+
+                        xDeform = Mathf.Clamp(xDeform, 0, maxDeform);
+                        yDeform = Mathf.Clamp(yDeform, 0, maxDeform);
+                        zDeform = Mathf.Clamp(zDeform, 0, maxDeform);
+
+                        Vector3 deform = new Vector3(xDeform, yDeform, zDeform);
+                        meshVerticies[i] -= deform * damageMultiplier;
+                    }
+                }
+            }
+
+            UpdateMeshVerticies();
+        }
+    }
+
+    void UpdateMeshVerticies()
+    {
+        filter.mesh.vertices = meshVerticies;
+        coll.sharedMesh = filter.mesh;
+    }
+}
---
+++ Scripts/DriftManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading.Tasks;
+
+public class DriftManager : MonoBehaviour
+{
+    public Rigidbody playerRB;
+    public TMP_Text totalScoreText;
+    public TMP_Text currentScoreText;
+    public TMP_Text factorText;
+    public TMP_Text driftAngleText;
+
+    private float speed=0;
+    private float driftAngle=0;
+    private float driftFactor=1;
+    private float currentScore;
+    private float totalScore;
+
+    private bool isDrifting = false;
+
+    public float minimumSpeed = 5;
+    public float minimumAngle = 10;
+    public float driftingDelay = 0.2f;
+    public GameObject driftingObject;
+    public Color normalDriftColor;
+    public Color nearStopColor;
+    public Color driftEndedColor;
+
+    private IEnumerator stopDriftingCoroutine = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        driftingObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageDrift();
+        ManageUI();
+    }
+    void ManageDrift()
+    {
+        speed = playerRB.velocity.magnitude;
+        driftAngle = Vector3.Angle(playerRB.transform.forward, (playerRB.velocity + playerRB.transform.forward).normalized);
+        if (driftAngle > 120)
+        {
+            driftAngle = 0;
+        }
+        if (driftAngle >= minimumAngle && speed > minimumSpeed)
+        {
+            if(!isDrifting||stopDriftingCoroutine!=null)
+            {
+                StartDrift();
+            }
+        }
+        else
+        {
+            if (isDrifting && stopDriftingCoroutine == null)
+            {
+                StopDrift();
+            }
+        }
+        if (isDrifting)
+        {
+            currentScore += Time.deltaTime * driftAngle * driftFactor;
+            driftFactor += Time.deltaTime;
+            driftingObject.SetActive(true);
+        }
+    }
+
+    async void StartDrift()
+    {
+        if (!isDrifting)
+        {
+            await Task.Delay(Mathf.RoundToInt(1000 * driftingDelay));
+            driftFactor = 1;
+        }
+        if (stopDriftingCoroutine != null)
+        {
+            StopCoroutine(stopDriftingCoroutine);
+            stopDriftingCoroutine = null;
+        }
+        currentScoreText.color = normalDriftColor;
+        isDrifting = true;
+    }
+    void StopDrift()
+    {
+        stopDriftingCoroutine = StoppingDrift();
+        StartCoroutine(stopDriftingCoroutine);
+    }
+    private IEnumerator StoppingDrift()
+    {
+        yield return new WaitForSeconds(0.1f);
+        currentScoreText.color = nearStopColor;
+        yield return new WaitForSeconds(driftingDelay * 4f);
+        totalScore += currentScore;
+        isDrifting = false;
+        currentScoreText.color = driftEndedColor;
+        yield return new WaitForSeconds(0.5f);
+        currentScore = 0;
+        driftingObject.SetActive(false);
+    }
+
+    void ManageUI()
+    {
+        totalScoreText.text = "Total: " + (totalScore).ToString("###,###,000");
+        factorText.text = driftFactor.ToString("###,###,##0.0")+"X";
+        currentScoreText.text=currentScore.ToString("###,###,000");
+        driftAngleText.text=driftAngle.ToString("###,##0") + "°";
+    }
+}
+
+
+
+
+
+
+
+
